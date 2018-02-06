bc7450c99be0e93ece434c98b3a971c734dd6d99
Ed Ilyin <ed.il@live.com>
2018-02-06 09:27:27 +0200

    test uaa

--- src/App/App.fs
+++ src/App/App.fs
@@ -1,84 +1,100 @@
 module App.View
 
-open App.State
+open Fable.Core
 open Elmish
-open Elmish.Browser.Navigation
-open Elmish.Browser.UrlParser
-open Elmish.Debug
-open Elmish.HMR
 open Elmish.React
+open Fable.Import.Browser
 open Fable.Helpers.React
 open Fable.Helpers.React.Props
-open Global
-open Types
-open Fable.Import.Browser
+open Fable.Core.JsInterop
 
 
-let footer =
-    div [ Id "footer"; ClassName "en" ] [
-        table [ FrameBorder "0"; CellSpacing "0"; CellPadding "0" ] [
-            tbody [] [
-                tr [] [
-                    td [ ClassName "cookie"; Id "cookieMessage" ] []
-                ]
-            ]
-        ]
-        div [ ClassName "container" ] [
-            div [ ClassName "col" ] [
-                h4 [] [
-                    sprintf
-                        "About %s Automatic Ink - Powered by Epson ReadyInk"
-                        brandName
-                        |> str
-                ]
-                p [] [
-                    sprintf
-                        "%s Automatic Ink - Powered by Epson ReadyInk is a pay-as-you-go, hassle-free automated print service to make sure your printer never runs out of ink."
-                        brandName
-                        |> str
-                ]
-            ]
-            div [ ClassName "col" ] []
-        ]
-    ]
+type Msal =
 
+type Model = {
+    msal: Msal option
+    user: User option
+}
+
+type Msg =
+    | Failure of System.Exception
+    | SignIn
+    | UpdateUser of string option
+    | SignOut
+
+
+let [<Import("*","msal")>] Msal: Msal = jsNative
+do printfn "global location: %A" window.location
+let b2cClientId = "0041318d-9770-47b5-be03-f9d7f94d2793"
+let b2cLogin = "https://login.microsoftonline.com/"
+let tenant = "automaticinkelkjop"
+
+let b2cAuthority policyName =
+    sprintf "%stfp/%s.onmicrosoft.com/B2C_1A_%s"
+        b2cLogin
+        tenant
+        policyName
+
+let msalApp p =
+    createNew Msal?UserAgentApplication (b2cClientId, b2cAuthority p)
+
+let getAndUpdateUser susi dispatch =
+    match !!susi?getUser() with
+        | None -> ()
+        | Some user -> UpdateUser !!user?name |> dispatch
+
+let init () =
+    { msal = None; user = None }
+        , Cmd.ofSub <| fun dispatch ->
+            let rec susi =
+                createNew Msal?UserAgentApplication (
+                    b2cClientId,
+                    b2cAuthority "signup_signin",
+                    fun _ ->
+                        do printfn "token callback"
+                        // let susi = msalApp "signup_signin"
+                        getAndUpdateUser susi dispatch
+                )
+            getAndUpdateUser susi dispatch
+
+
+let b2cScopes =
+    [| sprintf "https://%s.onmicrosoft.com/api/read" tenant |]
+
+let update msg model =
+    do printfn "update: %A" msg
+    match msg with
+        | Failure x ->
+            do printfn "failure: %s" x.Message
+            model, Cmd.none
+
+        | SignIn ->
+            model
+                , Cmd.ofSub <| fun dispatch ->
+                    let susi =
+                        createNew Msal?UserAgentApplication (
+                            b2cClientId,
+                            b2cAuthority "signup_signin",
+                            fun _ ->
+                                do printfn "token callback"
+                                let susi = msalApp "signup_signin"
+                                getUser susi dispatch
+                        )
+                    do !!susi?loginRedirect(b2cScopes, "brand=elkjop")
+
+        | UpdateUser user ->
+            match user with None -> "none" | Some u -> u
+            , Cmd.none
+
+        | SignOut -> model, Cmd.attemptFunc ()
 
 let root model dispatch =
-    let pageHtml =
-        function
-            | Page.Home ->
-                HomeMsg >> dispatch |> Home.View.root model.home
+    div [] [
+        div [] [ str model ]
+        button [ OnClick (fun _ -> dispatch SignIn ) ] [ str "sign in" ]
+        button [ OnClick (fun _ -> dispatch SignOut ) ] [ str "sign out" ]
+    ]
 
-            | Page.Account ->
-                AccountMsg
-                    >> dispatch
-                    |> Account.View.root model.account
-
-    div [ Id "wrapper"; sprintf "en %s>" brandName |> ClassName ]
-        [   str model.log
-            NavbarMsg >> dispatch |> Navbar.View.root model.navbar
-            pageHtml model.currentPage
-            footer
-        ]
-
-// App
-let hashParams =
-    window.location.hash.Substring(1).Split('&')
-        |> Seq.map toKeyValuePair
-        |> Seq.choose id
-        |> Map.ofSeq
-
-// do printfn "hash params: %A" hashParams
-
-// match Map.tryFind "id_token" hashParams with
-//     | None ->
 Program.mkProgram init update root
-|> Program.toNavigable (parseHash pageParser) urlUpdate
-#if DEBUG
-|> Program.withDebugger
-|> Program.withHMR
-#endif
 |> Program.withReact "app"
 |> Program.run
-
-    // | Some _ -> ()
